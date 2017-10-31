78a9cda059ba76417db4416c3b328180b77fbf66
Jon Grimes <jong@spiceworks.com>
2017-10-31 16:55:46 -0500

    Trying to get this to work

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ const MESSAGE: [u8; 49] = [0xA2, 0x48, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
                            0x00];
 
 fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind UDP socket");
+    let socket = UdpSocket::bind("0.0.0.0:34254").expect("Couldn't bind UDP socket");
     let mut buf: [u8; 1024] = [0; 1024];
     let ip = Ipv4Addr::new(10, 192, 4, 35);
     // socket.connect((ip, NET_BIOS_PORT)).expect("Couldn't connect UDP socket");
     socket.send_to(&MESSAGE, (ip, NET_BIOS_PORT)).ok().expect("Couldn't send data");
     println!("Waiting for response");
-    match socket.recv_from(&mut buf) {
-        Ok(received) => println!("Received {:?} bytes", received),
-        Err(e) => println!("recv failed: {:?}", e),
-    }
+    let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
+    println!("{} bytes received", number_of_bytes);
+    println!("{} source address", src_addr);
 }
