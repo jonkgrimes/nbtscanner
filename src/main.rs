f04a11076485f8a699d8ff845bc070d9ef870d2a
Jon Grimes <jong@spiceworks.com>
2018-01-14 12:09:23 -0600

    Starting to stub out structs and methods for NetBios packets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::time::{Duration};
 use std::net::UdpSocket;
 use std::net::Ipv4Addr;
+use std::fmt;
+use std::fmt::Display;
 
 const NET_BIOS_PORT: u16 = 137;
 const MESSAGE: [u8; 50] = [0xA2, 0x48, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -11,33 +13,75 @@ const MESSAGE: [u8; 50] = [0xA2, 0x48, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
 const RESPONSE_BASE_LEN: usize = 57;
 const RESPONSE_NAME_LEN: usize = 15;
 const RESPONSE_NAME_BLOCK_LEN: usize = 18;
+const TIMEOUT_SECONDS: u64 = 2;
 
+
+struct NetBiosPacket {
+    data: [u8; 1024]
+} 
+
+impl Display for NetBiosPacket {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut values = String::new();
+        for byte in self.data.iter() {
+            values.push_str(&format!("{:x}, ", byte));
+        }
+        write!(f, "[ {} ]", values)
+    }
+}
 
 fn main() {
-    for i in 1..255 {
-        let socket = UdpSocket::bind("0.0.0.0:34254").expect("Couldn't bind UDP socket");
-        socket.set_read_timeout(Some(Duration::new(5, 0))).ok();
+    for i in 35..36 {
+        let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind UDP socket");
+        socket.set_read_timeout(Some(Duration::new(TIMEOUT_SECONDS, 0))).ok();
         let mut buf: [u8; 1024] = [0; 1024];
         let ip = Ipv4Addr::new(10, 192, 4, i);
+        socket.connect((ip, NET_BIOS_PORT)).ok().expect("Couldn't connect to remote server");
         println!("Requesting info from {}", ip);
-        socket.send_to(&MESSAGE, (ip, NET_BIOS_PORT)).ok().expect("Couldn't send data");
+        socket.send(&MESSAGE).ok().expect("Couldn't send data");
         println!("Waiting for response");
-        match socket.recv_from(&mut buf) {
-            Ok((number_of_bytes, src_addr)) => {
+        match socket.recv(&mut buf) {
+            Ok(number_of_bytes) => {
+                let packet = NetBiosPacket { data: buf.clone() };
                 println!("{} bytes received", number_of_bytes);
-                println!("{} source address", src_addr);
-                let offset = RESPONSE_BASE_LEN + RESPONSE_NAME_LEN;
-                let name_range = RESPONSE_BASE_LEN..offset;
-                let name_bytes = &buf[name_range];
-                let block_range = offset..(offset + RESPONSE_NAME_BLOCK_LEN);
-                let block_bytes = &buf[block_range];
-                println!("Computer Name: {}", String::from_utf8_lossy(name_bytes));
-                println!("Block Name: {}", String::from_utf8_lossy(block_bytes));
+                println!("{} source address", ip);
+                println!("{}", packet);
+                println!("Computer Name: {}", get_name_from_data(&buf));
+                println!("Block Name: {}", get_block_from_data(&buf));
             },
             Err(error) => {
                 println!("Encountered and error when contacting {}: {:?}", ip, error);
                 continue;
             }
+        }
+    }
+}
+
+
+fn get_name_from_data(data: &[u8]) -> String {
+    let offset = RESPONSE_BASE_LEN + RESPONSE_NAME_LEN;
+    let name_range = RESPONSE_BASE_LEN..offset;
+    let name_bytes = Vec::from(&data[name_range]);
+
+    match String::from_utf8(name_bytes) {
+        Ok(name) => name,
+        Err(_) => {
+            println!("Couldn't decode the name");
+            String::from("N/A")
+        }
+    }
+}
+
+fn get_block_from_data(data: &[u8]) -> String {
+    let offset = RESPONSE_BASE_LEN + RESPONSE_NAME_LEN;
+    let block_range = offset..(offset + RESPONSE_NAME_BLOCK_LEN);
+    let block_bytes = Vec::from(&data[block_range]);
+
+    match String::from_utf8(block_bytes) {
+        Ok(name) => name,
+        Err(_) => {
+            println!("Couldn't decode the name block");
+            String::from("")
         }
     }
 }
