fe69c361e1cb60a365a161fb00be40ae4027eb12
Jon Grimes <jong@spiceworks.com>
2018-01-21 12:57:05 -0600

    A little formatting change

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::net::Ipv4Addr;
 use std::fmt;
 use std::fmt::Display;
 use std::str::FromStr;
+use std::thread;
+
 use clap::{App, Arg};
 
 const NET_BIOS_PORT: u16 = 137;
@@ -46,8 +48,10 @@ fn main() {
         .version("0.1")
         .author("Jon Grimes <jonkgrimes@gmail.com>")
         .about("Scans the given IP address range for NetBIOS information")
-        .arg(Arg::with_name("RANGE"))
-            .help("")
+        .arg(Arg::with_name("RANGE")
+            .help("The IP address/range")
+            .required(true)
+        )
         .get_matches();
 
     let raw_ip_str = matches.value_of("RANGE").unwrap();
@@ -59,27 +63,32 @@ fn main() {
         }
     };
 
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind UDP socket");
-    socket.set_read_timeout(Some(Duration::new(TIMEOUT_SECONDS, 0))).ok();
+    let handle = thread::spawn(move || {
+        // bind to port 0 and let the OS decide
+        let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind UDP socket");
+        // timeout after 2 seconds
+        socket.set_read_timeout(Some(Duration::new(TIMEOUT_SECONDS, 0))).ok();
 
-    let mut buf: [u8; 1024] = [0; 1024];
-    socket.connect((ip, NET_BIOS_PORT)).ok().expect("Couldn't connect to remote server");
-    println!("Requesting info from {}", ip);
-    socket.send(&MESSAGE).ok().expect("Couldn't send data");
-    println!("Waiting for response");
-    match socket.recv(&mut buf) {
-        Ok(number_of_bytes) => {
-            let packet = NetBiosPacket { data: buf.clone(), length: number_of_bytes };
-            println!("{} bytes received", number_of_bytes);
-            println!("{} source address", ip);
-            println!("{}", packet);
-            println!("Computer Name: {}", get_name_from_data(&buf));
-            println!("Block Name: {}", get_block_from_data(&buf));
-        },
-        Err(error) => {
-            println!("Encountered and error when contacting {}: {:?}", ip, error);
+        let mut buf: [u8; 1024] = [0; 1024];
+        socket.connect((ip, NET_BIOS_PORT)).ok().expect("Couldn't connect to remote server");
+        println!("Requesting info from {}", ip);
+        socket.send(&MESSAGE).ok().expect("Couldn't send data");
+        println!("Waiting for response");
+
+        match socket.recv(&mut buf) {
+            Ok(number_of_bytes) => {
+                let packet = NetBiosPacket { data: buf.clone(), length: number_of_bytes };
+                println!("{} bytes received", number_of_bytes);
+                println!("{}", packet);
+                println!("{}\t{}\t{}", ip, get_name_from_data(&buf), get_block_from_data(&buf));
+            },
+            Err(error) => {
+                println!("Encountered an error when contacting {}: {:?}", ip, error);
+            }
         }
-    }
+    });
+
+    handle.join();
 }
 
 
